74807ae51735671e2299efa9cf5ed8e9789d9008
alesanjuan123 <64500389+alesanjuan123@users.noreply.github.com>
2022-08-11 00:27:51 -0300

    Add files via upload
    Ejercicio numero 4 de Python de VALORACION FINAL DE MODULO.

---
+++ Ejercicio 4 python examen.py
@@ -0,0 +1,8 @@
+##ejercicio numero 4 : Crear un lista en Python denominado “Perro2”
+
+Perro2 = [14, "Fido", "12/12/2012", "Macho", 23546987]
+indice = 0
+for elementos in Perro2:
+    print(Perro2[indice -1])
+    indice -= 1
+    
