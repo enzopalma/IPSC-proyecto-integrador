e8dc4c5724968f6605724c210bf29d8cea019bcf
alesanjuan123 <64500389+alesanjuan123@users.noreply.github.com>
2022-08-13 15:36:21 -0300

    Add files via upload
    Ejercicio 9 base de datos

---
+++ ejercicio-9-bases-de-datos.sql
@@ -0,0 +1,5 @@
+
+-- 9. Escriba una consulta que permita actualizar la dirección de un dueño. Su nueva dirección es Libertad 123
+UPDATE Veterinaria.DUENO SET Direccion = 'Libertad 123' WHERE (DNI = '35671201');
+SELECT * FROM Dueno; 
+
