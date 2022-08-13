c5a208df5a78680c4fcc8141e661b4eb4221b57e
GCastelP <gcastelt@gmail.com>
2022-08-13 08:01:27 -0300

    Modificación de un registro de fecha_nac de animal en tabla perro

---
+++ ejercicio4.sql
@@ -0,0 +1,5 @@
+/* 4. Actualice la fecha de nacimiento de algún animal (perro) que usted considere
+
+UPDATE veterinaria.PERRO SET Fecha_nac = '2021-02-25' WHERE (ID_Perro = '3');
+
+SELECT * FROM Perro  
