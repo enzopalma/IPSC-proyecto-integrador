6f663e3d0e4acc8425a0fa6f5b65a895553496e4
= <=>
2022-08-14 13:49:10 -0300

    ejercicio 9 de python

---
+++ ejercicio9.py
@@ -0,0 +1,15 @@
+#9) Crear una tupla en Python con el nombre de “Historial5” la cual contenga los siguientes valores:
+
+#8520, 9510, 7530, 3570, 1590
+#Que representa montos de atención en pesos por diferentes servicios/consultas de la mascota “Toto”. Crear una función para determinar el valor máximo de atención gastada en “Toto”.
+
+
+Historial5=(8520, 9510, 7530, 3570, 1590)
+
+def Mayormonto (Historial5):
+
+ mayormonto=max(Historial5)
+ print("Lo que mas ha gastado Toto es",mayormonto)
+
+
+Mayormonto(Historial5)
